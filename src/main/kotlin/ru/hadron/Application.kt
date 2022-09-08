7dc32376efce9ec34b8c832f707ac2cf09ae0b38
hadron <hadron@yandex.ru>
2022-09-08 23:38:51 +0300

    add features

--- src/main/kotlin/ru/hadron/Application.kt
+++ src/main/kotlin/ru/hadron/Application.kt
@@ -1,12 +1,23 @@
 package ru.hadron
 
+import io.ktor.client.*
+import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.defaultheaders.*
+import io.ktor.server.routing.*
 import ru.hadron.plugins.*
+import io.ktor.server.plugins.contentnegotiation.*
 
-fun main(args: Array<String>): Unit =
-    io.ktor.server.netty.EngineMain.main(args)
-
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
-fun Application.module() {
-    configureRouting()
+fun Application.module(testing: Boolean = false) {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(Routing)
+    install(ContentNegotiation) {
+        gson {
+            setPrettyPrinting()
+        }
+    }
 }
