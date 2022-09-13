8207712a3e9f16f91b5711ea9a41d2a46ae9ce0f
hadron <hadron@yandex.ru>
2022-09-13 23:59:10 +0300

    add/update notes

--- src/main/kotlin/ru/hadron/plugins/routes/NoteRoutes.kt
+++ src/main/kotlin/ru/hadron/plugins/routes/NoteRoutes.kt
@@ -1,11 +1,16 @@
 package ru.hadron.plugins.routes
 
+import io.ktor.http.HttpStatusCode.Companion.BadRequest
+import io.ktor.http.HttpStatusCode.Companion.Conflict
 import io.ktor.http.HttpStatusCode.Companion.OK
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ru.hadron.data.collections.Note
 import ru.hadron.data.getNotesForUser
+import ru.hadron.data.saveNote
 
 fun Route.noteRoutes() {
     route("/getNotes") {
@@ -14,6 +19,24 @@ fun Route.noteRoutes() {
                 val email = call.principal<UserIdPrincipal>()!!.name
                 val notes = getNotesForUser(email)
                 call.respond(OK, notes)
+            }
+        }
+    }
+
+    route("/addNote") {
+        authenticate {
+            post {
+                val note = try {
+                    call.receive<Note>()
+                } catch (e: io.ktor.server.plugins.ContentTransformationException) {
+                    call.respond(BadRequest)
+                    return@post
+                }
+                if (saveNote(note)) {
+                    call.respond(OK)
+                } else {
+                    call.respond(Conflict)
+                }
             }
         }
     }
